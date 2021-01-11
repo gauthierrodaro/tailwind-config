44ed810ef5bf5f40d96edbfe2155e94402d7cc93
Gauthier Rodaro <gauthier.rodaro@gmail.com>
2021-01-11 15:37:31 +0100

    fix: add missing colors

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,26 +1,28 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
+const colors = require("tailwindcss/colors")
 
 module.exports = {
   darkMode: "class",
   theme: {
-    extend: {
-      colors: {
-        transparent: "transparent",
-        current: "currentColor",
-        gray: {
-          50: "#fafafa",
-          100: "#f4f4f5",
-          200: "#e4e4e7",
-          300: "#d4d4d8",
-          400: "#a1a1aa",
-          500: "#71717a",
-          600: "#52525b",
-          700: "#3f3f46",
-          800: "#27272a",
-          900: "#18181b",
-          950: "#111113",
-        },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      colors,
+      gray: {
+        50: "#fafafa",
+        100: "#f4f4f5",
+        200: "#e4e4e7",
+        300: "#d4d4d8",
+        400: "#a1a1aa",
+        500: "#71717a",
+        600: "#52525b",
+        700: "#3f3f46",
+        800: "#27272a",
+        900: "#18181b",
+        950: "#111113",
       },
+    },
+    extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
